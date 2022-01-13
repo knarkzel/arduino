4474b6bbf185280697865b11f217f7513a644d11
Knarkzel <knarkzel@gmail.com>
2022-01-13 22:49:00 +0100

    latest

--- v/main.v
+++ v/main.v
@@ -1,5 +1,31 @@
-module main
+#define EMPTY_STRUCT_DECLARATION
+#define VV_LOCAL_SYMBOL
+#define voidptr void*
+#define array
+#define _vcleanup  main__v_cleanup
+#define _vinit     main__v_init
+
+fn C.printf(&char, ...voidptr) int
+
+[markused]
+fn v_init(argc int, argv &&char) {
+	C.printf(c'v_init start\n')
+	C.printf(c'  argc: %d\n', argc)
+	C.printf(c'  argv: %p\n', argv)
+	C.printf(c'  argv[0]: %s\n', unsafe { argv[0] })
+	C.printf(c'--------------------------------------\n')
+}
+
+[markused]
+fn v_cleanup() {
+	C.printf(c'--------------------------------------\n')
+	C.printf(c'v_cleanup done\n')
+}
+
+fn abc(x int, y int) int {
+	return x + y
+}
 
 fn main() {
-	println('Hello World!')
+	C.printf(c'abc(): %d\n', abc(2, 3))
 }
